d4ea7a48db66f756d4aa46086453ee9bfb15e130
VictorMarcal <victormarcal90@hotmail.com>
2014-12-02 17:18:23 +0000

    struct casa
    struct casa

--- Monopolio/Monopolio/main.c
+++ Monopolio/Monopolio/main.c
@@ -1,5 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+
+
+
+typedef struct rc // registo de casa
+{
+	int posição;
+	char nome[50];
+	int preço;
+
+} Casa;
+
+
 
 int main(){
 
